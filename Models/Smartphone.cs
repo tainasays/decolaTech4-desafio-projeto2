31033d752e4e2dcb9a714a034138b22f0793df2d
tainasays <thaynasant.ana@hotmail.com>
2023-12-26 11:42:26 -0300

    feat: envio da primeira versão do projeto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo;
+        private string Imei;
+        private int Memoria;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("\t Smartphone iPhone");
+Iphone iphone = new Iphone(numero: "11966875521", modelo: "A22", imei: "758415414", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.Write("\n");
+
+Console.WriteLine("\t Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "81977462212", modelo: "1110", imei: "747548254", memoria: 1);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
